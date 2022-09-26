7b0350d21850c0ad752e49d31c3cc9afb7b5d489
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-09-26 10:50:30 +0300

    Implemented & Configured TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'texture' : "url('../public/assets/bg-pattern-header.svg)",
+      },
+      fontFamily: {
+        'sans' : ['Nunito Sans', ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+}
