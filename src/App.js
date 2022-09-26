a6bd1387139ac9ae937d597daf7de9ff8b947b81
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-09-26 22:18:00 +0300

    Created the initial style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import {GoSearch} from "react-icons/go";
+import {MdLocationOn} from "react-icons/md";
 
 function App() {
   return (
-    <div className="bg-sky-400 w-60 h-60">
-
-    </div>
+    <div className="w-full h-screen">
+        <div className="w-full h-72 bg-texture bg-cover bg-no-repeat flex items-center justify-center">
+            <div className="lg:w-2/3 w-4/5 relative">
+                <img className="w-44 h-12 inline-block" src={'../assets/logo.svg'} alt="logo" />
+                <div className="w-full absolute top-24">
+                    <div className="w-full h-32 bg-white rounded-lg space-x-2 flex justify-center items-center">
+                        <GoSearch className="lg:ml-0 ml-2 text-4xl text-indigo-400" />
+                        <input
+                            className="lg:w-1/3 w-1/4 p-6 text-xl text-slate-800"
+                            placeholder="Filter by title, companies, expertise..."
+                        />
+                        <MdLocationOn className="text-4xl text-indigo-400" />
+                        <input
+                            className="lg:w-1/3 w-1/4 p-6 text-xl text-slate-500"
+                            placeholder="Filer by location..."
+                        />
+                        <input
+                            className="w-6 h-6 text-gray-600"
+                            name="fullTime"
+                            type="checkbox"
+                        />
+                        <label className="text-xl font-bold pr-4" htmlFor="fullTime">
+                            Full time only
+                        </label>
+                        <button className="lg:px-10 px-4 p-4 text-white bg-indigo-500 rounded-lg font-bold text-xl ml-4">
+                            Search
+                        </button>
+                    </div>
+                </div>
+             </div>
+         </div>
+     </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'texture' : "url('../public/assets/bg-pattern-header.svg)",
-      },
-      fontFamily: {
-        'sans' : ['Nunito Sans', ...defaultTheme.fontFamily.sans],
+        'texture': "url('../public/assets/bg-pattern-header.svg')",
       }
     },
-  },
+      fontFamily: {
+        'sans' : ['Nunito Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
   plugins: [],
 }
